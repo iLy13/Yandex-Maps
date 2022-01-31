e249dbd858a90bd2a0f9f173d035c6fb66f5ff2d
iLy13 <egor.korneeko@gmail.com>
2022-01-31 21:45:00 +0700

    Вроде бы координаты по клику

--- main.py
+++ main.py
@@ -28,6 +28,7 @@ def load_map(server, params, name):
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
+width, height = screen.get_width(), screen.get_height()
 load_map(map_api_server, map_params, "map1.png")
 
 running = True
@@ -37,7 +38,11 @@ while running:
         if event.type == pygame.QUIT:
             pygame.quit()
             os.remove('map1.png')
-        if event.type == pygame.KEYDOWN:
+        if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+            x, y = pygame.mouse.get_pos()
+            mx, my = width - x, height - y
+            coords = map_params['ll'][0], map_params['ll'][1]
+            koefx, koefy = mx / float(coords[0]), my / float(coords[1])
             if event.key == pygame.K_0:
                 map_params = {
                     "ll": get_ll_spn(toponym_to_find)[0],
