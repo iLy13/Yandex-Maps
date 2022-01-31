c0d528233f6dcdd5377cbc10698990e545ffabd9
darifedoreev <98749377+darifedoreev@users.noreply.github.com>
2022-01-31 20:44:17 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import os
 import sys
 import pygame
 import requests
-from functions import get_ll_spn, get_coordinates, get_nearest_object, geocode
+from yandex_functions import get_ll_spn, get_coordinates, get_nearest_object, geocode
 
 toponym_to_find = "Красноярск, ул. Ленина, 114"
 map_api_server = "http://static-maps.yandex.ru/1.x/"
@@ -28,13 +28,38 @@ def load_map(server, params, name):
 
 pygame.init()
 screen = pygame.display.set_mode((600, 450))
+load_map(map_api_server, map_params, "map1.png")
 
 running = True
+
 while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             os.remove('map1.png')
-    load_map(map_api_server, map_params, "map1.png")
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_0:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "sat"
+                }
+                load_map(map_api_server, map_params, "map1.png")
+            elif event.key == pygame.K_1:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "map"
+                }
+                load_map(map_api_server, map_params, "map1.png") 
+            if event.key == pygame.K_2:
+                map_params = {
+                    "ll": get_ll_spn(toponym_to_find)[0],
+                    "spn": get_ll_spn(toponym_to_find)[1],
+                    "l": "sat,trf,skl"
+                }
+                load_map(map_api_server, map_params, "map1.png")            
+                
     screen.blit(pygame.image.load('map1.png'), (0, 0))
     pygame.display.flip()
+    
